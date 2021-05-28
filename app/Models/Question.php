dc9ff63a57b55db554f64d74c3e0a5590a2fcf6f
RUFAI Ahmed <lamydas.ng@gmail.com>
2021-05-28 14:50:35 +0100

    General Question model via artisan model generator

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+    protected $fillable = ['title','body']
+    public  function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function questions(){
+        returns $this->hasMany(Question::class);
+
+    }   
+    public function setTitleAttribute($value){
+         $this->attributes['title'] = $value;
+         $this->attributes['slug'] = str::slug($value);
+
+    }   
 }
