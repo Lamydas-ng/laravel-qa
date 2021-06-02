f67dba7e26031a90bd89bc76d30fd151655849ea
RUFAI Ahmed <lamydas.ng@gmail.com>
2021-06-02 15:18:13 +0100

    Add author info and question creation date in question items

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,5 +43,12 @@ class User extends Authenticatable
     public function questions()
     {
         return $this->hasMany(Question::class);
-    }   
+    }  
+    
+    public function getUrlAttribute()
+    {
+        //return route("questions.show", $this->id);
+       return '#';
+    }
+
 }
