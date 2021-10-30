bb71914c6bf22a6d73f5e8c0996028cf83286fde
Ahmed RUFAI <lamydas.ng@gmail.com>
2021-10-30 16:05:35 +0100

    Implement DRY principle on Views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -1,12 +1,12 @@
-<div class="row justify-content-center mt-4">
-     <div class="col-md-12">
+<div class="row mt-4">
+    <div class="col-md-12">
         <div class="card">
             <div class="card-body">
                 <div class="card-title">
                     <h2>{{ $answersCount . " " . str_plural('Answer', $answersCount) }}</h2>
                 </div>
                 <hr>
-                 @include ('layouts._message')
+                @include ('layouts._message')
 
                 @foreach ($answers as $answer)
                     <div class="media">
@@ -35,25 +35,7 @@
                                 <input type="hidden" name="vote" value="-1">
                             </form>
 
-                            @can ('accept', $answer)
-                                <a title="Mark this answer as best answer"
-                                    class="{{ $answer->status }} mt-2"
-                                    onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
-                                    >
-                                    <i class="fas fa-check fa-2x"></i>
-                                </a>
-                                <form id="accept-answer-{{ $answer->id }}" action="{{ route('answers.accept', $answer->id) }}" method="POST" style="display:none;">
-                                    @csrf
-                                </form>
-                            @else
-                                @if ($answer->is_best)
-                                    <a title="The question owner accepted this answer as best answer"
-                                        class="{{ $answer->status }} mt-2"
-                                        >
-                                        <i class="fas fa-check fa-2x"></i>
-                                    </a>
-                                @endif
-                            @endcan
+
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
@@ -74,15 +56,10 @@
                                 </div>
                                 <div class="col-4"></div>
                                 <div class="col-4">
-                                    <span class="text-muted">Answered {{ $answer->created_date }}</span>
-                                    <div class="media mt-2">
-                                        <a href="{{ $answer->user->url }}" class="pr-2">
-                                            <img src="{{ $answer->user->avatar }}">
-                                        </a>
-                                        <div class="media-body mt-1">
-                                            <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-                                        </div>
-                                    </div>
+                                    @include ('shared._author', [
+                                        'model' => $answer,
+                                        'label' => 'answered'
+                                    ])
                                 </div>
                             </div>
                         </div>
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -8,63 +8,42 @@
                 <div class="card-body">
                     <div class="card-title">
                         <div class="d-flex align-items-center">
-                             <h1> {{ $question->title }} </h1>
+                            <h1>{{ $question->title }}</h1>
                             <div class="ml-auto">
                                 <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
                             </div>
                         </div>
                     </div>
-                    <hr />
+
+                    <hr>
+
                     <div class="media">
-                        <div class="d-flex flex-column vote-controls pr-4">
-                            <a  title="This question useful"  href="" class="vote-up">
-                                <i class="fas fa-caret-up fa-3x"></i>
-                            </a>
-                            <span class="votes-count">1235</span>
-                            <a title="This question is not useful" href="" class="vote-down off">
-                                <i class="fas fa-caret-down fa-3x"></i>
-                            </a>
+                        @include ('shared\_vote', [
+                            'model' => $question
+                        ])
 
-                            <a title = "Click to add as favorite question (click again to undo)" href="" class="favorite mt-2  {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
-                                    onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
-                                    >
-                               <i class="fas fa-star fa-2x"></i>
-                            </a>
-                            <span class="favorites-count">{{ $question->favorites_count}}</span>
-                            <form id="favorite-question-{{ $question->id }}" action="/questions/{{ $question->id }}/favorites" method="POST" style="display:none;">
-                                @csrf
-                                @if ($question->is_favorited)
-                                    @method ('DELETE')
-                                @endif
-                            </form>
-                        </div>
-                      <div class="media-body">
-
-                            {!! $question->body_html  !!}`
-                            <div class="float-right">
-                                <span class="text-muted">Created {{ $question->created_date}}</span>
-                                    <div class="media mt-2">
-                                        <a href="{{$question->user->url}}" class="pr-2">
-                                            <img src="{{ $question->user->avatar }}" />
-                                       </a>
-                                       <div class="media-body mt-1">
-                                           <a href="{{$question->user->url}}" class="">{{ $question->user->name}}</a>
-                                       </div>
-                                    </div>
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="row">
+                                <div class="col-4"></div>
+                                <div class="col-4"></div>
+                                <div class="col-4">
+                                    @include ('shared._author', [
+                                        'model' => $question,
+                                        'label' => 'asked'
+                                    ])
+                                </div>
                             </div>
-                       </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
-
-    @include('answers._index',[
+    @include ('answers._index', [
         'answers' => $question->answers,
         'answersCount' => $question->answers_count,
     ])
-    @include('answers._create')
-
+    @include ('answers._create')
 </div>
 @endsection
